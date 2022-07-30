b2f660ab05cf49c77166409c117a9b7255474c81
Meryem Yuksel <myuksel@ufl.edu>
2022-07-29 22:06:21 -0400

    Score finished. Console implementation done

--- rockpaper.js
+++ rockpaper.js
@@ -1,6 +1,11 @@
-let choice = document.querySelector('button');
-let button = document.querySelector('button');
-button.addEventListener('click',onClick);
+
+let playerscore=0;
+let computerscore=0;
+
+let choice = document.querySelector('button')
+let button = document.querySelector('button')
+button.addEventListener('click',onClick)
+
 
 function onClick(){
 document.getElementById("result").innerHTML ="you win";
@@ -11,29 +16,39 @@ return compchoice;
 }
 function playRound(compChoice, playerChoice){
     compChoice =getcomputerChoice();
+    let result;
     if(compChoice ==playerChoice){
-        return("Its a tie!");
+        result ="Its a tie!";
     }
     else if(compChoice==0 && playerChoice ==1){
-        return("You Win! Paper beats rock. >:^)");
+        result="You Win! Paper beats rock. >:^)";
+        ++playerscore;
     }
     else if(compChoice ==0 && playerChoice ==2){
-        return("Loss.... Rocks beats scissors >:^(");
+        result="Loss.... Rocks beats scissors >:^(";
+        ++computerscore;
     }
     else if(compChoice ==1 && playerChoice ==0){
-        return("A terrible loss... Paper beats Rock :,(");
+        result="A terrible loss... Paper beats Rock :,(";
+        ++computerscore;
     }
     else if(compChoice ==1 && playerChoice ==2){
-        return("A grand WIN! Scissors beats Paper :D");
+        result="A grand WIN! Scissors beats Paper :D";
+        ++playerscore;
     }
     else if(compChoice ==2 && playerChoice ==0){
-        return("NICE! Rock beats Scissors :)");
+        result="NICE! Rock beats Scissors :)";
+        ++playerscore;
     }
     else{
-        return("Pity :( Scissors cuts paper");
+        result="Pity :( Scissors cuts paper";
+        ++computerscore;
     }
+    return result;
 }
 function game(){
+    playerscore=0;
+    computerscore=0;
     console.log("Let the game BEGIN!");
     for(let i=0; i<5; i++){
         let comp = getcomputerChoice();
@@ -56,7 +71,12 @@ function game(){
             i--;
         }
         else{
-        console.log(playRound(comp,playerchose));
+        let statement = playRound(comp,playerchose);
+        console.log(statement);
+        if(statement == "Its a tie!"){
+            i--;
+        }
+        console.log(`Your Score: ` + playerscore + `\n Computer Score: ` + computerscore);
         }
     }
 }
